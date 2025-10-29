fa748c6996ed16bbe72738ab5a4172dd022ab29a
xziying <xziying@vip.qq.com>
2025-10-29 11:51:26 +0800

    遗漏字段

--- card_cdapter.py
+++ card_cdapter.py
@@ -64,6 +64,8 @@ class CardAdapter:
         "name",  # 顶层字段
         "body",  # 顶层字段
         "flavor",  # 顶层字段
+        "card_back.other",  # 嵌套字段
+        "card_back.other",  # 嵌套字段
         "back.card_back.other",  # 嵌套字段
         "back.card_back.option",  # 嵌套字段
         "back.name",
